3675ce982b28aee00a20c0eb4082e3fbc089b164
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-28 18:48:09 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,7 +17,16 @@ class _HomePageState extends State<HomePage> {
           padding: EdgeInsets.all(20),
           child: ListView(
             children: [
-              
+              CircularPercentIndicator(
+                radius: 80,
+                backgroundColor: Colors.yellow,
+                progressColor: Colors.blue,
+                animateToInitialPercent: true,
+                animation: true,
+                animationDuration: 10000,
+                percent: 0.3,
+                circularStrokeCap: CircularStrokeCap.round,
+              ),
             ],
           ),
         ),
@@ -24,3 +34,13 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
